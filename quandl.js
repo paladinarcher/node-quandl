5e53d492124d149aea10889508ab6cbe5c643e2e
aBitNomadic <aBitNomadic01@gmail.com>
2017-02-18 00:24:12 -0500

    derp, forgot to update the prototype.

--- quandl.js
+++ quandl.js
@@ -6,7 +6,7 @@ function Quandl(config){
 }
 
 Quandl.prototype.configure = function(config){
-    this.auth_token = config.auth_token || undefined;
+    this.api_key = config.api_key || undefined;
     this.proxy = config.proxy || undefined;
     this.api_version = config.api_version || 3;
     this.api_version = ["v", this.api_version].join("");
